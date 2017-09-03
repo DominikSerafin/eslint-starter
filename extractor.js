d6f8eda69713cb536ef591875b509bcad068af94
Dominik Serafin <DominikSerafin@users.noreply.github.com>
2017-09-03 18:08:25 +0200

    Create extractor.js

---
+++ extractor.js
@@ -0,0 +1,20 @@
+function scrapeRules(id){
+	
+	var rules = [];
+	var container = $(id + ' ~ table.rule-list').eq(0);
+	
+	var ruleEl = container.find('tr');
+	
+	$(ruleEl).each(function(index, el){
+		var name = $(el).find('td').eq(2).text().trim();
+		var desc = $(el).find('td').eq(3).text().trim();
+		rules.push(`"${name}": 0,  // ${desc}`);
+	});
+	
+	return rules;
+	
+
+}
+
+var stylisticRules = scrapeRules('#stylistic-issues');
+console.log(stylisticRules.join('\r\n'));
