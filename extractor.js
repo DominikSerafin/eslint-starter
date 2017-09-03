fabad98391c5b016ee3c9e6c72e4ac80053cb798
Dominik Serafin <DominikSerafin@users.noreply.github.com>
2017-09-03 18:19:55 +0200

    Update extractor.js

--- extractor.js
+++ extractor.js
@@ -3,16 +3,23 @@ function scrapeRules(id){
 
   var rules = [];
   var container = $(id + ' ~ table.rule-list').eq(0);
-
+  var columnsBeforeComment = 32;
   var ruleEl = container.find('tr');
 
   $(ruleEl).each(function(index, el){
+
     var name = $(el).find('td').eq(2).text().trim();
     var desc = $(el).find('td').eq(3).text().trim();
-    rules.push(`"${name}": 0,  // ${desc}`);
+
+    var commentSpaces = columnsBeforeComment - name.length;
+    var spacesString = ' '.repeat(commentSpaces);
+
+    rules.push(`"${name}": 0,${spacesString}// ${desc}`);
+    
   });
 
   return rules;
+
 }
 
 var stylisticRules = scrapeRules('#stylistic-issues');
